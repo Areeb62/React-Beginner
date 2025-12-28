bd69c57c17cb693247ecf999689e4194ab291261
Areeb62 <runr62099@gmail.com>
2025-12-28 20:28:36 +0500

    Finsished project

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { useLoaderData } from 'react-router'
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/areeb62')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setData(data)
-            })
-    }, [])
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/areeb62')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             setData(data)
+    //         })
+    // }, [])
     return (
-        <div>Github followers: {data.followers}
+        <div>Github username: {data.login}
             <img src={data.avatar_url} alt="" width={300} />
         </div>
     )
 }
 
 export default Github
+
+export const githubInfo = async () => {
+    const response = await fetch('https://api.github.com/users/areeb62')
+    return response.json()
+}
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/Github/Github.jsx'
+import Github, {githubInfo} from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -34,17 +34,17 @@ import Github from './components/Github/Github.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />}/>
-      <Route path='about' element={<About />}/>
-      <Route path='contact' element={<Contact />}/>
-      <Route path='user/:userId' element={<User />}/>
-      <Route path='github' element={<Github />}/>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userId' element={<User />} />
+      <Route loader={githubInfo} path='github' element={<Github />} />
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
